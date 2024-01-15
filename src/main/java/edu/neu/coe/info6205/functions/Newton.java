2a5e658d5f6a61f91bcda8c957a3cf1ae29413e6
vedantdalvi <dalvi.v@northeastern.edu>
2024-01-15 16:41:52 -0500

    Assignmnet 0- Newton Rhapson Approximation

--- src/main/java/edu/neu/coe/info6205/functions/Newton.java
+++ src/main/java/edu/neu/coe/info6205/functions/Newton.java
@@ -5,9 +5,11 @@ import java.util.function.DoubleFunction;
 /**
  * This class models the Newton-Raphson Approximation algorithm.
  * See https://en.wikipedia.org/wiki/Newton%27s_method
- * It is an example of a non-deterministic algorithm inasmuch as the convergence (or lack thereof) is very dependent
+ * It is an example of a non-deterministic algorithm inasmuch as the convergence
+ * (or lack thereof) is very dependent
  * on the value of the initial guess x0 to the solve method.
- * However, if you run it with identical starting conditions, it will always come out the same: it does not use any random elements.
+ * However, if you run it with identical starting conditions, it will always
+ * come out the same: it does not use any random elements.
  */
 public class Newton {
 
@@ -30,10 +32,14 @@ public class Newton {
      * Method to solve this Newton problem.
      *
      * @param x0        the initial estimate of x.
-     *                  If this is too far from any root, the solution may not converge.
-     * @param maxTries  the maximum number of tries before admitting defeat due to non-convergence.
-     * @param tolerance the required precision for the value of f(x) to be considered equal to zero.
-     * @return either a Double (the actual root) or a String explaining why no root could be found.
+     *                  If this is too far from any root, the solution may not
+     *                  converge.
+     * @param maxTries  the maximum number of tries before admitting defeat due to
+     *                  non-convergence.
+     * @param tolerance the required precision for the value of f(x) to be
+     *                  considered equal to zero.
+     * @return either a Double (the actual root) or a String explaining why no root
+     *         could be found.
      */
     public Either<String, Double> solve(final double x0, final int maxTries, final double tolerance) {
         double x = x0;
@@ -41,12 +47,15 @@ public class Newton {
         for (; tries > 0; tries--)
             try {
                 final double y = f.apply(x);
-                if (Math.abs(y) < tolerance) return Either.right(x);
+                if (Math.abs(y) < tolerance)
+                    return Either.right(x);
                 x = x - y / dfbydx.apply(x);
             } catch (Exception e) {
-                return Either.left("Exception thrown solving " + equation + "=0, given x0=" + x0 + ", maxTries=" + maxTries + ", and tolerance=" + tolerance + " because " + e.getLocalizedMessage());
+                return Either.left("Exception thrown solving " + equation + "=0, given x0=" + x0 + ", maxTries="
+                        + maxTries + ", and tolerance=" + tolerance + " because " + e.getLocalizedMessage());
             }
-        return Either.left(equation + "=0 did not converge given x0=" + x0 + ", maxTries=" + maxTries + ", and tolerance=" + tolerance);
+        return Either.left(equation + "=0 did not converge given x0=" + x0 + ", maxTries=" + maxTries
+                + ", and tolerance=" + tolerance);
     }
 
     public static void main(String[] args) {
@@ -66,6 +75,22 @@ public class Newton {
                 aDouble -> {
                     // Publish the happy news.
                     System.out.println("Good news! " + newton.equation + " was solved: " + aDouble);
+                });
+
+        // Newton's Approximation problem: e^x - 3x = 0
+        Newton newton1 = new Newton("e^x - 3x", x -> Math.exp(x) - 3 * x, x -> Math.exp(x) - 3);
+
+        // Solve the problem starting with a value of x = 1;
+        // requiring a precision of 10^-7;
+        // and giving up after 200 tries.
+        Either<String, Double> result1 = newton.solve(1.0, 200, 1E-7);
+
+        // Process the result
+        result1.apply(
+                System.err::println,
+                aDouble -> {
+                    // Publish the happy news.
+                    System.out.println("Good news! " + newton1.equation + " was solved: " + aDouble);
                 });
     }
 
